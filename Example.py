9bc7993e27432930273b9a8113765dbc5189d34d
JaydenMBurrows <jaydenborrows@gmail.com>
2025-02-08 10:34:30 -0500

    added lines

--- Example.py
+++ Example.py
@@ -1,5 +1,5 @@
-print("Hello World")
 print("Welcome to learning how to use GIT")
 
+print("Today's date is the 8th of February, 2025, and it's currently 10:32am")
 # On the left side, there should be a "Source Control" tab
 # --- Click on it ---
