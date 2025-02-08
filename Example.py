cb966985f26fbdc949313a49864e9efb79ea57a1
Priya <priyasampat.college@gmail.com>
2025-02-08 10:34:54 -0500

    added more lines

--- Example.py
+++ Example.py
@@ -1,5 +1,7 @@
 print("Hello World")
 print("Welcome to learning how to use GIT")
+print("Today's date is Feb 8th 2025 and it's currently ")
+
 
 # On the left side, there should be a "Source Control" tab
 # --- Click on it ---
